cdb67c62fa787f141df07dd1de6cfb4f2c9aee69
Emlyn Corrin <Emlyn.Corrin@microsoft.com>
2017-02-26 00:50:48 +0000

    Improve FX (mainly sparkle)

--- fx.py
+++ fx.py
@@ -1,8 +1,8 @@
-import random
 import spectra
 from PIL import Image
 from bisect import bisect_right
 from numbers import Number
+from random import choice, randrange
 
 
 def gradient(size, colours):
@@ -114,11 +114,15 @@ class ChaseFX(FXBase):
 
 
 class FlashFX(FXBase):
-    def __init__(self, size, time, *colours):
+    def __init__(self, size, *args):
         super().__init__(size)
         self.start_t = None
-        self.time = time
-        self.img = gradient(size, colours)
+        if len(args) > 0 and isinstance(args[0], Number):
+            self.time = args[0]
+            args = args[1:]
+        else:
+            self.time = 0
+        self.img = gradient(size, args)
         self.trans = Image.new('RGBA', size, (0, 0, 0, 0))
 
     def getDisplay(self, time, previous):
@@ -126,31 +130,51 @@ class FlashFX(FXBase):
             self.start_t = time
         if time > self.start_t + self.time:
             return None
+        if self.time <= 0:
+            return self.img
         a = (time - self.start_t) / self.time
         return Image.blend(self.img, self.trans, a)
 
 
 class SparkleFX(FXBase):
-    def __init__(self, size, time, fade, nspark, *colours):
+    def __init__(self, size, *args):
         super().__init__(size)
         self.start_t = None
-        self.time = time
-        self.fade = fade
-        self.nspark = nspark
-        self.colours = colours if len(colours) > 0 else ['white']
+        self.prev_t = None
+        if len(args) > 0 and isinstance(args[0], Number):
+            self.time = args[0]
+            args = args[1:]
+        else:
+            self.time = 0
+        if len(args) > 0 and isinstance(args[0], Number):
+            self.fade = args[0]
+            args = args[1:]
+        else:
+            self.fade = 0
+        if len(args) > 0 and isinstance(args[0], Number):
+            self.nspark = args[0]
+            args = args[1:]
+        else:
+            self.nspark = 10
         self.colours = [spectra.html(c).color_object.get_upscaled_value_tuple()
-                        for c in self.colours]
+                        for c in (args if len(args) > 0 else ['white'])]
         self.img = Image.new('RGBA', size, (0, 0, 0, 0))
         self.trans = Image.new('RGBA', size, (0, 0, 0, 0))
 
     def getDisplay(self, time, previous):
         if self.start_t is None:
             self.start_t = time
-        if time > self.start_t + self.time * 2:
+        if time > self.start_t + self.time + self.fade:
             return None
-        self.img = Image.blend(self.img, self.trans, self.fade)
+        pix = self.img.load()
+        if self.prev_t is not None:
+            nfade = 255 if self.fade <= 0 else round(255 * (time - self.prev_t) / self.fade)
+            for y in range(self.size[1]):
+                for x in range(self.size[0]):
+                    p = pix[x, y]
+                    pix[x, y] = p[:3] + (max(0, p[3] - nfade),)
         if time < self.start_t + self.time:
-            pix = self.img.load()
             for i in range(self.nspark):
-                pix[random.randrange(self.size[0]), random.randrange(self.size[1])] = random.choice(self.colours)
+                pix[randrange(self.size[0]), randrange(self.size[1])] = choice(self.colours)
+        self.prev_t = time
         return self.img
